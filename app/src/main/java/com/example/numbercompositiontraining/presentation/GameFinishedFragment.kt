be16bafbe6fb72adf3fae6ce1a1b9b2deae3540f
per-sever <a.petrin107@gmail.com>
2022-11-24 19:01:55 +0300

    serializable interface changed to parcelable

--- app/src/main/java/com/example/numbercompositiontraining/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/numbercompositiontraining/presentation/GameFinishedFragment.kt
@@ -43,13 +43,17 @@ class GameFinishedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object :
+        val callback = object :
             OnBackPressedCallback
                 (true) {
             override fun handleOnBackPressed() {
                 retryGame()
             }
-        })
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+        binding.buttonRetry.setOnClickListener {
+            retryGame()
+        }
     }
 
     override fun onDestroyView() {
@@ -58,7 +62,9 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun parseArgs() {
-        gameResult = requireArguments().getSerializable(KEY_GAME_RESULT) as GameResult
+        requireArguments().getParcelable<GameResult>(KEY_GAME_RESULT)?.let {
+            gameResult = it
+        }
     }
 
     companion object {
@@ -68,7 +74,7 @@ class GameFinishedFragment : Fragment() {
         fun newInstance(gameResult: GameResult): GameFinishedFragment {
             return GameFinishedFragment().apply {
                 arguments = Bundle().apply {
-                    putSerializable(KEY_GAME_RESULT, gameResult)
+                    putParcelable(KEY_GAME_RESULT, gameResult)
                 }
             }
         }
